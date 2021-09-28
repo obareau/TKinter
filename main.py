4231eecc990f6fca50aaa2aebad8eb88ae73a8aa
obareau <olivier.bareau@gmail.com>
2021-09-28 22:24:19 +0200

    dictionary syntax or configure function ?

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ class MyApp:
         """ We can not make it any bigger than that """
         root.maxsize(800,600)
         
-        """We put the window first then parameters"""
+        """ We put the window first then parameters """
         label = Label(root, text="Some label text")
         
-        """let's make this labal appear for real"""
+        """ let's make this labal appear for real """
         label.pack()
+        
+        """ Dictionary syntax """"
+        # label["text"] = "New Label Text"
+        # label["font"] = ("Courier")
+        
+        """ configure function is quicker when ther's a lot of params to change i a row """
+        label.configure(text = "New Label Text :-)", font=("Helvetica", 30))
         
 """ let's create root """
 root = Tk()

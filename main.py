3c2a424b7daeb4bb139caf32def2a6bc45e8b8a1
obareau <olivier.bareau@gmail.com>
2021-09-28 21:28:35 +0200

    adding a label to the window

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from tkinter import *
 
 class MyApp:
     
+    """Initialiser"""
     def __init__(self, root):
+        
         root.title("My app")
 
         """ let's define our window """
         root.geometry("500x400")
 
-        """ We can not make it any bigger thant that """
+        """ We can not make it any bigger than that """
         root.maxsize(800,600)
-
+        
+        """We put the window first then parameters"""
+        label = Label(root, text="Some label text")
+        
+        """let's make this labal appear for real"""
+        label.pack()
+        
+""" let's create root """
 root = Tk()
 """ Let's create an instance of MyApp called root"""
 MyApp(root)

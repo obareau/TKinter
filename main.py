e2348f19e4b6d8bf8e6f3a6c392c98c122729d00
obareau <olivier.bareau@gmail.com>
2021-09-28 21:18:20 +0200

    Let's create an instance of MyApp called root

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from tkinter import *
 
+class MyApp:
+    
+    def __init__(self, root):
+        root.title("My app")
+
+        """ let's define our window """
+        root.geometry("500x400")
+
+        """ We can not make it any bigger thant that """
+        root.maxsize(800,600)
+
 root = Tk()
-root.title("My app")
+""" Let's create an instance of MyApp called root"""
+MyApp(root)
+
+
 root.mainloop()

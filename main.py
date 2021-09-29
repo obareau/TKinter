76591037ffb451af3ddbdc1148ea00e361f5e1c7
obareau <olivier.bareau@gmail.com>
2021-09-29 19:05:18 +0200

    let's use grid positioning system

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class MyApp:
         """ We put the window first then parameters """
         label = Label(root, text="Some label text",
                       textvariable=self.label_text, bg="lightblue")
-
+        label.grid(column=1, row=1)
         """ let's make this label appear for real """
         # label.pack(side=tk.LEFT, padx=10, pady=5, )
         """Widgets are aligned left to right with a padding between it 
@@ -31,7 +31,8 @@ class MyApp:
         # label["text"] = "New Label Text"
         # label["font"] = ("Courier")
         # ? Or this one ?
-        """ configure function is quicker when ther's a lot of params to change in a row """
+        """ configure function is quicker when there's 
+        a lot of params to change in a row """
         label.configure(text="New Label Text :-)", font=("Helvetica", 30))
 
         self.entry_text = StringVar()
@@ -39,16 +40,20 @@ class MyApp:
         # packing goes top to botton so this widget will be below the first one
         # entry.pack(side=tk.LEFT)
         # ! We will use get & set later !
-        entry.place(x = 100, y = 50)
+        entry.grid(column=2, row=1)
+        # entry.place(x = 100, y = 50)
         # label["textvariable"] = entry_text
+        
         button = Button(root, text="Submit", command=self.press_button)
         # button.pack(side=tk.LEFT)
-
+        button.grid(column=1, row=2)
+        
         self.list_items_strings = ["hey", "hi",
                                    "hello", "greetings"]  # let's make a field
         list_items = StringVar(value=self.list_items_strings)
         listbox = Listbox(root, listvariable=list_items)
         # listbox.pack(side=tk.LEFT, padx=40, pady=20, )
+        listbox.grid(column=2, row=2)
         listbox["height"] = 3  # listbox by default contains 10 rows
         listbox.bind("<<ListboxSelect>>", lambda s: self.select_item(
             listbox.curselection()))  # ! We have a tuple
@@ -62,7 +67,8 @@ class MyApp:
     def select_item(self, index):
         # ! Tuple here so we need to add a zero
         selected_item = self.list_items_strings[index[0]]
-        print(selected_item)  # Just a test
+        print(selected_item) 
+        # print(self.list_items_strings[index[0]])# Just a test
 
 
 # ! let's create root
